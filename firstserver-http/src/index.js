6598a9a5c2f59f12699de576cd2f199204aeab67
caiocipriano <caio.silvax@hotmail.com>
2024-05-20 20:56:13 -0300

    feat: const route condition to show users with route object

--- firstserver-http/src/index.js
+++ firstserver-http/src/index.js
@@ -1,16 +1,19 @@
 const http = require('http');
-const users = require('./mocks/users');
+
+const routes = require('./routes')
 
 const server = http.createServer((request, response)=> {
-
     console.log(`Request method: ${request.method} | Endpoint: ${request.url}`)
 
-    if(request.url === '/users' && request.method === 'GET'){
-        response.writeHead(200,{'Content-Type':'application/json'})
-        response.end(JSON.stringify(users))
+    const route = routes.find((routeObj) => (
+        routeObj.endpoint === request.url && routeObj.method === request.method
+    ));
+
+    if (route){
+        route.handler(req,res)
     }else{
-        response.writeHead(404,{'Content-TYPE':'text/html'})
-        response.end(`Cannot ${request.method} ${request.url}`)
+        response.writeHead(404, {'Content-Type' : 'application/json'})
+        response.end(`Cannot ${request.url} ${request.method}`)
     }
 })
 
