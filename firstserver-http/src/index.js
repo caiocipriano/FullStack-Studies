d74958ed4b91bce9b98fa7d1ff28dd1cceaeac23
caiocipriano <caio.silvax@hotmail.com>
2024-05-21 14:18:46 -0300

    feat: using URL to refactor a route

--- firstserver-http/src/index.js
+++ firstserver-http/src/index.js
@@ -1,16 +1,17 @@
 const http = require('http');
-
+const { URL } = require('url')
 const routes = require('./routes')
 
 const server = http.createServer((request, response)=> {
-    console.log(`Request method: ${request.method} | Endpoint: ${request.url}`)
+    const parseUrl = new URL (`http://localhost:8080${request.url}`)
+    console.log(`Request method: ${request.method} | Endpoint: ${parseUrl.pathname}`)
 
     const route = routes.find((routeObj) => (
-        routeObj.endpoint === request.url && routeObj.method === request.method
+        routeObj.endpoint === parseUrl.pathname && routeObj.method === request.method
     ));
 
     if (route){
-        route.handler(req,res)
+        request.query = Object.fromEntries(parseUrl.searchParams)
     }else{
         response.writeHead(404, {'Content-Type' : 'application/json'})
         response.end(`Cannot ${request.url} ${request.method}`)
