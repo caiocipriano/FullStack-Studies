5d4d5646d6887bd9a1eb8a43088930a0ea3728aa
caiocipriano <caio.silvax@hotmail.com>
2024-05-27 10:12:14 -0300

    feat: ContactRepository

---
+++ node-express/my-contacts/src/app/repositories/ContactRepository.js
@@ -0,0 +1,28 @@
+const {v4} = require('uuid');
+
+const contacts = [
+    {
+        id:v4(),
+        name:"Caio",
+        phone:"1231312312",
+        category_id:v4()
+    },
+    {
+        id:v4(),
+        name:"Fulano",
+        phone:"1231312312",
+        category_id:v4()
+    }
+];
+
+
+class ContactRepository{
+     findAll(){
+        return new Promise((resolve, reject)=> resolve(contacts))
+    }
+    findById(){
+        return new Promise((resolve)=> resolve(contacts.find((contacts)=>contacts.id === id)))
+    }
+}
+
+module.exports = new ContactRepository()
